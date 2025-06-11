db8c05479f88890aee5992c0adf282b7b96a884d
Pikin <pikin@bk.ru>
2025-06-11 16:38:20 +0500

    add temp directory, add clear temp after creating barcodes, change barcode size, font size, border size

--- barcodeApp.py
+++ barcodeApp.py
@@ -1,5 +1,5 @@
 import os
-import tempfile
+import shutil
 import tkinter as tk
 from tkinter import messagebox, filedialog
 from barcode import Code128
@@ -7,15 +7,12 @@ from barcode.writer import ImageWriter
 from PIL import Image as PilImage
 from PIL import ImageDraw, ImageFont
 import pandas as pd
-from reportlab.lib.units import cm
-from reportlab.lib.pagesizes import inch, landscape, letter
-from reportlab.lib.utils import ImageReader
-from reportlab.platypus import SimpleDocTemplate, Image as RlImage, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Image as RlImage
 
 counter = 0
 
 # Настройки шрифта и размеров
-font_size = 40
+font_size = 65
 font_type = "arial.ttf"
 img_width_cm = 10
 img_height_cm = 5
@@ -25,13 +22,37 @@ img_width_px = int(img_width_cm * px_per_inch)
 img_height_px = int(img_height_cm * px_per_inch)
 
 
+def clear_temp_directory():
+    temp_dir = 'temp'
+    try:
+        for root, dirs, files in os.walk(temp_dir, topdown=False):
+            for file in files:
+                os.remove(os.path.join(root, file))
+            for dir in dirs:
+                shutil.rmtree(os.path.join(root, dir))
+    except Exception as e:
+        print(f"Ошибка при очистке папки: {e}")
+
+
 def generate_barcode(code_number):
     global counter
+
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+
     ean = Code128(str(code_number), writer=ImageWriter())
-    filename = f'barcode_{counter}.png'
+    filename = f'barcode_{counter}'
+
+    full_path = os.path.join('temp', filename)
+    write_options = {
+        'module_width': 0.5,  # Ширина одного элемента штрихкода (чем меньше значение, тем тоньше линии)
+        'module_height': 10,  # Высота штрихкода в пикселях
+        'font_size': 12,  # Размер шрифта внизу штрихкода
+        'text_distance': 5  # Расстояние между штрихкодом и текстом снизу
+    }
+    ean.save(full_path, write_options)
     counter += 1
-    full_path = ean.save(filename)
-    return full_path
+    return full_path + '.png'
 
 
 def create_template(barcode_image_path, article, name, unit):
@@ -39,7 +60,7 @@ def create_template(barcode_image_path, article, name, unit):
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(font_type, size=font_size)
 
-    text_y_offset = 10
+    text_y_offset = 0
     text_bbox = font.getbbox(article)
     text_width = text_bbox[2] - text_bbox[0]
     center_x = (img.width - text_width) // 2
@@ -61,7 +82,7 @@ def create_template(barcode_image_path, article, name, unit):
     if current_line.strip():
         lines.append(current_line.strip())
 
-    y_pos = text_y_offset + font_size * 1.5
+    y_pos = (text_y_offset + 10) + font_size * 1.5
     for i, line in enumerate(lines[:3]):
         text_bbox = font.getbbox(line)
         text_width = text_bbox[2] - text_bbox[0]
@@ -73,29 +94,31 @@ def create_template(barcode_image_path, article, name, unit):
     text_bbox = font.getbbox(unit)
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] - text_bbox[1]
-    right_bottom_position_x = img.width - text_width - 200
-    right_bottom_position_y = img.height - text_height - 200
+    right_bottom_position_x = img.width - text_width - 100
+    right_bottom_position_y = img.height - text_height - 100
     draw.text((right_bottom_position_x, right_bottom_position_y),
               f"{unit}",
               fill="black",
-              font=ImageFont.truetype(font_type, size=100))
+              font=ImageFont.truetype(font_type, size=70))
 
     # Штрих-код снизу слева
     bc_img = PilImage.open(barcode_image_path)
-    scale_factor = 1.5
-    new_bc_size = (int(bc_img.width * scale_factor), bc_img.height)
+    scale_factor_width = 1   #1.8
+    scale_factor_height = 1   #1.1
+    new_bc_size = (int(bc_img.width * scale_factor_width), int(bc_img.height * scale_factor_height))
     resized_bc = bc_img.resize(new_bc_size, resample=PilImage.LANCZOS)
-    position_x = 20
-    position_y = img.height - resized_bc.height - 10
+    position_x = 0
+    position_y = img.height - resized_bc.height + 45
     img.paste(resized_bc, (position_x, position_y))
     template_filename = f'template_{counter}.png'
-    img.save(template_filename)
-    return template_filename
+    full_path = os.path.join('temp', template_filename)
+    img.save(full_path)
+    return full_path
 
 
 def save_images_to_pdf(image_paths, output_pdf_path):
-    page_width = 1380  # Надо поработать с размерами
-    page_height = 780   # Надо поработать с размерами
+    page_width = 1339  # Переход к более мелким размерам приводит к падению.
+    page_height = 746  # Более крупные размеры уменьшают макет.
 
     doc = SimpleDocTemplate(output_pdf_path, pagesize=(page_width, page_height))  # Задание нужного размера страницы
     elements = []
@@ -125,6 +148,7 @@ def on_generate_click():
                             f'Шаблон с изображением штрих-кода и информацией\nуспешно сохранён в файле:\n{"output.pdf"}')
     except Exception as err:
         messagebox.showerror('Ошибка', str(err))
+    clear_temp_directory()
 
 
 def open_excel_file():
@@ -164,8 +188,12 @@ def open_excel_file():
             # messagebox.showinfo('Готово!', 'Этикетки успешно собраны в PDF.')
         else:
             messagebox.showwarning('Предупреждение', 'Нет валидных записей для создания этикеток.')
-        messagebox.showinfo('Готово!', 'Этикетки успешно собраны в PDF.')
+    messagebox.showinfo('Готово!', 'Этикетки успешно собраны в PDF.')
+    clear_temp_directory()
 
+
+if not os.path.exists('temp'):
+    os.mkdir('temp')
 
 # Интерфейс программы
 root = tk.Tk()
